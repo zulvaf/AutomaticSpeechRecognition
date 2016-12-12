1ba64353180509e3f7de10751b331dc1710029a7
dininta <dinintaannisa@yahoo.com>
2016-12-12 15:21:34 +0700

    Make mfcc

---
+++ makeMfcc.pl
@@ -0,0 +1,24 @@
+use File::Basename;
+
+my $filename = 'targetlist.txt';
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+
+$dir = "./sounds/*";
+my @files = glob( $dir );
+
+foreach (@files ){
+	$folderName = basename($_);
+  foreach my $fp (glob("$_/*.wav")) {
+  	$filename = fileparse($fp, qr/\.[^.]*/);
+  	print $fh $fp;
+	  print $fh " mfcc/";
+	  print $fh $folderName;
+	  print $fh "/";
+	  print $fh $filename;
+	  print $fh ".mfcc\n";
+  }
+}
+
+close $fh or die "can't read close '$fp': $OS_ERROR";
+
+system("HCopy -A -D -C analysis.conf -S targetlist.txt");
